bb07ac936e49d8723583a91fc033c4862ab2e490
Alexander Chocron <alchocro@microsoft.com>
2018-07-23 16:17:56 -0700

    Change solution type for api service

---
+++ backend/BikeHack/BikeHack/Controllers/BikesController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using BikeHack.Models;
+
+namespace BikeHack.Controllers
+{
+    [Route("bikes")]
+    public class BikesController : Controller
+    {
+        [HttpPost("{bikeId}/status")]
+        public IActionResult PostStatus([FromRoute] string bikeId)
+        {
+            return Ok();
+        }
+
+        [HttpGet("{bikeId}/status")]
+        public IActionResult GetStatus([FromRoute] string bikeId)
+        {
+            return Ok();
+        }
+
+        [HttpPost("{bikeId}/checkout")]
+        public IActionResult CheckOut([FromRoute] string bikeId)
+        {
+            return Ok();
+        }
+
+        [HttpPost("{bikeId}/checkin")]
+        public IActionResult CheckIn([FromRoute] string bikeId)
+        {
+            return Ok();
+        }
+       
+        [HttpGet]
+        public IActionResult GetAllBikeIds([FromQuery] string state)
+        {
+            return Ok();
+        }
+    }
+}
--- backend/bike-hack-api-service/bike-hack-api-service/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="bike_hack_api_service.WebApiApplication" Language="C#" %>
