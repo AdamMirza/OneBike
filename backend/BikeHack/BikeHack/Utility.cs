a46c024547b8efb30fa28094c2abf413db4ebab2
Alexander Chocron <alchocro@microsoft.com>
2018-07-24 14:42:35 -0700

    Remove error view model

---
+++ backend/BikeHack/BikeHack/Utility.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BikeHack
+{
+    public class Utility
+    {
+        public static double MilesBetweenCoordinates(double latitudeA, double longitudeA, double latitudeB, double longitudeB)
+        {
+            return 0;
+        }
+    }
+}
