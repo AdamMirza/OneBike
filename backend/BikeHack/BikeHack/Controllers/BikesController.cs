29e835b158ba41ad1ac3360a2c95f1795ce48305
Alexander Chocron <alchocro@microsoft.com>
2018-07-25 10:42:24 -0700

    Implement bulk of bikes controller

--- backend/BikeHack/BikeHack/Controllers/BikesController.cs
+++ backend/BikeHack/BikeHack/Controllers/BikesController.cs
@@ -6,7 +6,6 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using BikeHack.Models;
 using Microsoft.WindowsAzure.Storage;
-using Microsoft.WindowsAzure.Storage.Table;
 
 namespace BikeHack.Controllers
 {
@@ -21,37 +20,36 @@ namespace BikeHack.Controllers
             Utility.LogMessage("Started bikes controller successfully.");
         }
 
-        [HttpPost("{bikeId}/status")]
-        public async Task<IActionResult> PostStatus([FromRoute] Guid bikeId, [FromBody] BikeStatus status)
+        [HttpPost]
+        public async Task<IActionResult> CreateBike([FromBody] BikeStatus status)
+        {
+            Utility.LogMessage("CreateBike request.");
+            var bike = await _bikeStorage.CreateBikeAsync(status);
+            return CreatedAtAction(nameof(GetStatus), new { bikeId = bike.BikeId }, bike);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetBikeIds([FromQuery] string state)
+        {
+            BikeState? bikeState = state != null ? new BikeState?(Enum.Parse<BikeState>(state)) : null;
+            var bikes = await _bikeStorage.GetBikesAsync(bikeState);
+            var bikeIds = bikes.Select(bike => bike.BikeId);
+            return Ok(bikeIds);
+        }
+
+        [HttpPatch("{bikeId}")]
+        public async Task<IActionResult> UpdateBikeStatus([FromRoute] Guid bikeId, [FromBody] BikeStatus status)
         {
             //TODO add to trip distance if bike is on a trip
             await _bikeStorage.UpdateBikeStatusAsync(bikeId, status);
             return Ok();
         }
 
-        [HttpGet("{bikeId}/status")]
+        [HttpGet("{bikeId}")]
         public async Task<IActionResult> GetStatus([FromRoute] Guid bikeId)
         {
             var bike = await _bikeStorage.RetrieveBikeAsync(bikeId);
             return Ok(bike.GetStatus());
-        }
-
-        [HttpPost("{bikeId}/startTrip")]
-        public IActionResult StartTrip([FromRoute] Guid bikeId)
-        {
-            return Ok();
-        }
-
-        [HttpPost("{bikeId}/endTrip")]
-        public IActionResult EndTrip([FromRoute] Guid bikeId)
-        {
-            return Ok();
-        }
-       
-        [HttpGet]
-        public IActionResult GetAllBikeIds([FromQuery] string state)
-        {
-            return Ok();
         }
     }
 }
--- backend/BikeHack/BikeHack/Storage/BikeStorage.cs
+++ backend/BikeHack/BikeHack/Storage/BikeStorage.cs
@@ -2,6 +2,7 @@
 using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Table;
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace BikeHack
@@ -9,12 +10,23 @@ namespace BikeHack
     public class BikeStorage
     {
         private const string TableName = "bikes";
+        private const string StatePropertyName = "BikeState";
+        private const string PartitionKeyPropertyName = "PartitionKey";
         private CloudTable _table;
 
         public BikeStorage(CloudStorageAccount account)
         {
             var client = account.CreateCloudTableClient();
             _table = client.GetTableReference(TableName);
+        }
+
+        public async Task<Bike> CreateBikeAsync(BikeStatus status)
+        {
+            var bikeId = Guid.NewGuid();
+            var bike = new Bike(status, bikeId);
+            var operation = TableOperation.Insert(bike);
+            await _table.ExecuteAsync(operation);
+            return bike;
         }
 
         public async Task UpdateBikeStatusAsync(Guid bikeId, BikeStatus status)
@@ -29,6 +41,29 @@ namespace BikeHack
             var retrieveOperation = TableOperation.Retrieve<Bike>(Guid.Empty.ToString(), bikeId.ToString());
             var result = await _table.ExecuteAsync(retrieveOperation);
             return result.Result as Bike;
+        }
+
+        public async Task<IEnumerable<Bike>> GetBikesAsync(BikeState? state)
+        {
+            TableQuery<Bike> query = new TableQuery<Bike>
+            {
+                FilterString = TableQuery.GenerateFilterCondition(PartitionKeyPropertyName, QueryComparisons.Equal, Guid.Empty.ToString())
+            };
+            if (state.HasValue)
+            {
+                var stateFilter = TableQuery.GenerateFilterCondition(StatePropertyName, QueryComparisons.Equal, state.Value.ToString());
+                query.FilterString = TableQuery.CombineFilters(query.FilterString, TableOperators.And, stateFilter);
+            }
+
+            TableContinuationToken token = null;
+            var bikes = new List<Bike>();
+            do
+            {
+                var querySegment = await _table.ExecuteQuerySegmentedAsync(query, token);
+                token = querySegment.ContinuationToken;
+                bikes.AddRange(querySegment.Results);
+            } while (token != null);
+            return bikes;
         }
     }
 }
